rotation warp generation fix flow file name indexing off by one

examples/rotation_warp_image.py
@@ -70,8 +70,8 @@ if __name__ == '__main__':
     last_sim_t = time.time()
 
     frame_id = 0
-    optical_flow_rot_last = warp.discrete_optical_flow_due_to_rotation(0, 0, 0, focal_length, image.shape)
-    last_warped_image = np.zeros(image.shape).astype(np.uint8)
+    optical_flow_rot_last = None
+    last_warped_image = None
 
     # The simulation will be limited to this virtual speed in hz
     for sim_t in iter(TimeIterator(sim_rate_hz=60)):
@@ -89,13 +89,22 @@ if __name__ == '__main__':
                                 ypr[0], ypr[1], 0,
                                 focal_length, image.shape)
         warped_image = warp.image_warp(image, optical_flow_rot)
+
+        if last_warped_image is None:
+            last_warped_image = warped_image
+            optical_flow_rot_last = optical_flow_rot
+            file_name = 'image_{:06d}.png'.format(frame_id)
+            cv2.imwrite(os.path.join(args.save_dir, file_name), warped_image)
+            frame_id += 1
+            continue
+
         intermediate_flow = optical_flow_rot - optical_flow_rot_last
 
         if args.save_dir:
             file_name = 'image_{:06d}.png'.format(frame_id)
             cv2.imwrite(os.path.join(args.save_dir, file_name), warped_image)
 
-            flo_name = 'flow_{:06d}.flo'.format(frame_id)
+            flo_name = 'flow_{:06d}.flo'.format(frame_id-1)
             write_flo(intermediate_flow, os.path.join(args.save_dir, flo_name))
             frame_id += 1
 